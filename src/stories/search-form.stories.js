86b2305c6b0caf7527b3ecfcedb9401609845108
Suraj Kentura <Suraj_Kentura@epam.com>
2024-03-24 12:22:09 +0530

    Implement storybook for existing components

---
+++ src/stories/search-form.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import SearchForm from '../components/search-form/index';
+
+export default {
+  title: 'SearchForm',
+  component: SearchForm,
+};
+
+export const SearchFormStory = () => <SearchForm />;
